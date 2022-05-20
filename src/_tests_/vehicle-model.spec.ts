ecd26bcd803927b96d81d3213b1e000df3b6ef5c
Deiby Dayans R <dramoslance@gmail.com>
2022-05-20 19:03:56 -0400

    refactor: add VehicleModelHelper inject constructor

--- src/_tests_/vehicle-model.spec.ts
+++ src/_tests_/vehicle-model.spec.ts
@@ -2,10 +2,18 @@ import { VehicleModel } from '../models/vehicle-model'
 
 describe('Classes', () => {
   it('Class vehicleModel', () => {
-    const vehicleModel = new VehicleModel('Nissan', 'DXT', 2020)
+    const vehicleModel = new VehicleModel({
+      name: 'Nissan',
+      model: 'DXT',
+      year: 2020,
+      price: 25,
+      inventory: false
+    })
 
     expect(vehicleModel.getName()).toEqual('Nissan')
     expect(vehicleModel.getModel()).toEqual('DXT')
     expect(vehicleModel.getYear()).toBe(2020)
+    expect(vehicleModel.getPrice()).toBe(25)
+    expect(vehicleModel.getInventory()).toBe(false)
   })
 })
