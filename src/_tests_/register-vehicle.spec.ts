73bc8ad01042913e26b855fb76b495a25bdadb5e
Isaac Mendoza <isaacgtr2000@gmail.com>
2022-05-22 18:31:46 -0500

    test: add RegisterVehicle color and year

--- src/_tests_/register-vehicle.spec.ts
+++ src/_tests_/register-vehicle.spec.ts
@@ -6,9 +6,10 @@ describe('Register Vehicle', () => {
 
     const httpRequest = {
       body: {
-        // name: 'Nissan',
         model: 'DXT',
-        year: 2020
+        year: 2020,
+        color: 'color'
+        // name: 'Nissan'
       }
     }
 
@@ -23,13 +24,49 @@ describe('Register Vehicle', () => {
     const httpRequest = {
       body: {
         name: 'Nissan',
+        year: 2020,
+        color: 'color'
         // model: 'DXT',
-        year: 2020
       }
     }
 
     const httpResponse = sut.handle(httpRequest)
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('error in the: model'))
+  })
+
+  test('if the year does not exists return 400', () => {
+    const sut = new RegisterVehicle()
+
+    const httpRequest = {
+      body: {
+        name: 'Nissan',
+        model: 'DXT',
+        color: 'color'
+        // year: 2020
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('error in the: year'))
+  })
+
+  test('if the color does not exist return 400', () => {
+    const sut = new RegisterVehicle()
+
+    const httpRequest = {
+      body: {
+        name: 'Nissan',
+        model: 'DXT',
+        year: 2020
+        // color: 'color'
+      }
+    }
+
+    const httpResponse = sut.handle(httpRequest)
+
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('error in the: color'))
   })
 })
