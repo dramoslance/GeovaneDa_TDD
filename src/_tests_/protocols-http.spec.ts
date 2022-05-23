3dbbfe24d3f89dbe0c0b52ded3dd9732994980a3
Isaac Mendoza <isaacgtr2000@gmail.com>
2022-05-23 17:55:40 -0500

    test: add test for Http Protocols

---
+++ src/_tests_/protocols-http.spec.ts
@@ -0,0 +1,41 @@
+import { UrlLogin, UrlUser } from '../protocols/protocols-http'
+
+describe('Protocols Http and Queries', () => {
+  test('Url login', () => {
+    const parseUrl = UrlLogin.parseUrl('http://localhost:3000/login')
+
+    expect(parseUrl.href).toBe('http://localhost:3000/login')
+    expect(parseUrl.port).toBe('3000')
+  })
+
+  test('Response query', () => {
+    const parseUrl = UrlLogin.parseUrl('http://localhost:3000/login?user=user&password=password')
+
+    const expectedParams = {
+      user: 'user',
+      password: 'password'
+    }
+
+    expect(parseUrl.query).toEqual(expectedParams)
+  })
+
+  test('Url user', () => {
+    const parseUrl = UrlUser.parseUrl('http://localhost:3000/user')
+
+    expect(parseUrl.href).toBe('http://localhost:3000/user')
+  })
+  // http://localhost:3000/user
+
+  test('User response query', () => {
+    const parseUrl = UrlUser.parseUrl('http://localhost:3000/user?user=user&password=password&name=name&lastname=lastname')
+
+    const expectedQuery = {
+      user: 'user',
+      password: 'password',
+      name: 'name',
+      lastname: 'lastname'
+    }
+    expect(parseUrl.query).toEqual(expectedQuery)
+  })
+  // http://localhost:3000/user?user=user&password=password&name=name&lastname=lastname
+})
