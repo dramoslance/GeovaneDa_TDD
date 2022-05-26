9bbd77604c6d2b3df546adf31a7075b3a36a334e
Isaac Mendoza <isaacgtr2000@gmail.com>
2022-05-26 17:36:07 -0500

    refactor: add InvalidArgument in UrlLogin

--- src/protocols/protocols-http.ts
+++ src/protocols/protocols-http.ts
@@ -1,9 +1,13 @@
 /* eslint-disable @typescript-eslint/no-extraneous-class */
 // eslint-disable-next-line node/no-deprecated-api
 import { UrlWithParsedQuery, parse } from 'url'
+import { InvalidArgument } from '../errors/invalid-arg'
 
 export class UrlLogin {
   public static parseUrl (url: string): UrlWithParsedQuery {
+    if (url === undefined || url === '') {
+      throw new InvalidArgument('')
+    }
     return parse(url, true)
   }
 }
