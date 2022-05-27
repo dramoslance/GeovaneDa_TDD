9f133463abab79fce2ac21d8bcbab5626ed0f188
Deiby Ramos <dramoslance@gmail.com>
2022-05-27 14:55:52 -0500

    test: add FoodHelper

--- src/_tests_/food-model.spec.ts
+++ src/_tests_/food-model.spec.ts
@@ -1,3 +1,4 @@
+import { FoodHelper } from '../helpers/food-model-helper'
 import { FoodModel } from '../models/food-model'
 
 describe('Classes', () => {
@@ -14,5 +15,11 @@ describe('Classes', () => {
     expect(foodModel.getDescription()).toEqual('This is cow meat')
     expect(foodModel.getPrice()).toBe(50)
     expect(foodModel.getInventory()).toBe(50)
+  })
+
+  it('Food Helper', () => {
+    const foodHelper = new FoodHelper()
+
+    expect(foodHelper).toEqual(foodHelper)
   })
 })
