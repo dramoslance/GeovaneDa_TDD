5b318a021cdfe43eb95412c05c629455a43e6a73
Deiby Ramos <dramoslance@gmail.com>
2022-05-27 18:15:12 -0500

    test: implements open-closed principal PrimeContract

--- src/_tests_/contract-model.spec.ts
+++ src/_tests_/contract-model.spec.ts
@@ -1,4 +1,4 @@
-import { ContractModel } from '../models/contract-model'
+import { ContractModel, PrimeContract } from '../models/contract-model'
 
 describe('Class Contract', () => {
   it('Contract 12 month', () => {
@@ -16,4 +16,16 @@ describe('Class Contract', () => {
     expect(contract.getMonth()).toBe(12)
     expect(contract.getMonthlyCost()).toBe(500)
   })
+})
+
+describe('monthlyDiscount Prime contract 24 months', () => {
+  const contract = new PrimeContract({
+    id: '#001',
+    date_init: new Date(2019),
+    expiration_date: new Date(2021),
+    month: 24,
+    monthly_cost: 500
+  })
+
+  expect(contract.getMonthlyDiscount()).toBe(50)
 })
