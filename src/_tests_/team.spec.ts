320ef7b558bfb33c8c48102f3e35c93059278d80
Deiby Ramos <dramoslance@gmail.com>
2022-05-31 18:54:27 -0500

    test: add Team position

---
+++ src/_tests_/team.spec.ts
@@ -0,0 +1,15 @@
+import { Team } from '../models/team-model'
+
+describe('Team Class', () => {
+  test('Position', () => {
+    const team = new Team()
+
+    const position = {
+      name: 'goalkeeper',
+      tShirt: 1,
+      skills: ['Kick hard', 'Jump fast', 'Jump high']
+    }
+
+    expect(team.position(position.name, position.tShirt, position.skills)).toEqual(['goalkeeper', 1, ['Kick hard', 'Jump fast', 'Jump high']])
+  })
+})
