b27a08e1f049855de697dd0a648ab1e55a8f24e1
Deiby Ramos <dramoslance@gmail.com>
2022-06-01 20:45:28 -0500

    feat: add TeamModel position

--- src/interfaces/ITeam.ts
+++ src/interfaces/ITeam.ts
@@ -1,3 +1,6 @@
+export interface ISoccerPlayer {
+  attacker: (name: string) => string
+}
 export interface ITeam {
   position: (name: string, tShirt: number, skills: string[]) => any[]
 }
--- src/models/team-model.ts
+++ src/models/team-model.ts
@@ -1,11 +1,16 @@
-import { ITeam } from '../interfaces/ITeam'
+import { ITeam, ISoccerPlayer } from '../interfaces/ITeam'
 
-export class Team implements ITeam {
+export class Team implements ITeam, ISoccerPlayer {
   position (name: string, tShirt: number, skills: string[]): any[] {
     return [
       name,
       tShirt,
       skills
     ]
+  }
+
+  attacker (name: string): string {
+    name = 'attacker'
+    return name
   }
 }
